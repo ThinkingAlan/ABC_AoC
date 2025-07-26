f4a361bdb9759b33128ae6cbf1da282855743d0a
Alan Carabes <132530785+ThinkingAlan@users.noreply.github.com>
2025-07-26 14:17:51 -0700

    Add some changes to 2015_day2

--- src/2015_day2.cpp
+++ src/2015_day2.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <fstream>
 
+#include <iostream>
+
 int main()
 {
     // Need to get input
@@ -9,13 +11,25 @@ int main()
     input_text.open("input.txt", std::ios::in);
     
     std::string input;
+    std::vector<std::string> in_vec;
     if (input_text.is_open())
     {
         while (getline(input_text, input))
         {
-            // Put into vector`
+            // Put into vector
+            in_vec.push_back(input);
         }
         input_text.close();
+    }
+
+    /*
+        Starting part 1 
+    */
+
+    std::string tmp_str;
+    for (auto v : in_vec)
+    {
+        tmp_str = v;
     }
 
     return 0;
